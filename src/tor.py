13d0a1b6815cc0bca20c184db249c536bbed1f83
Bueezi <ulrich.ben@hotmail.com>
2024-11-23 01:26:31 +0100

    Changed Tor.py to fix the chrome download issue

--- src/tor.py
+++ src/tor.py
@@ -1,7 +1,11 @@
 # Credits : Xolo/@efenatuyo
 # Notice : this code aswell as "Tor.exe" is not part of our Lisence.
 
-import os, subprocess, psutil, time
+# U might be concerned with the Tor.exe file, it is the official Tor executable that can be download off of the tor website : 
+# https://archive.torproject.org/tor-package-archive/torbrowser/13.0.16/tor-expert-bundle-windows-x86_64-13.0.16.tar.gz
+# File's SHA256 : 66fd723d0dd219807c6d7dcc331e25c8d05adccf4a66312928fbe1d0e45670ed
+
+import os, subprocess, psutil, requests
 
 class ServiceInstaller:
     def __init__(self, amount, proxy_start_port):
@@ -9,7 +13,7 @@ class ServiceInstaller:
         self.proxy_start_port = proxy_start_port
         self.stop_tor_windows()
 
-    def _generate_ips_file(self, config_path):
+    def generate_config(self, config_path):
         tor_config = f"SOCKSPort {self.proxy_start_port}\n"
         # tor_config += "ExitNodes {fr},{de},{nl}\nExcludeExitNodes {us},{cn},{ru}\n" download geoip files if u wanna use this
         tor_config += "BandwidthRate 1GB\nBandwidthBurst 1GB\n"
@@ -17,11 +21,24 @@ class ServiceInstaller:
             tor_config += f"HTTPTunnelPort {self.proxy_start_port + i+1}\n"
         with open(config_path, 'w') as f:
             f.write(tor_config)
+        
+    def download_tor(self):
+        tor_url = "https://github.com/Bueezi/ZeFinder-Roblox-Group-Finder/raw/2fa8c1f08a5ab3a30946a97708b8f5566f10a621/src/tor/tor.exe" # We use the version of a commit so that the exe stays the same even if we change it in the repo
+        response = requests.get(tor_url)
+        if response.status_code != 200:
+            return None
+        exe_path = "src\\tor\\tor.exe"
+        with open(exe_path, 'wb') as f:
+            f.write(response.content)
 
     def install_service(self):
         exe_path = "src\\tor\\tor.exe"
         config_path = "src\\tor\\config"
-        self._generate_ips_file(config_path)
+        self.generate_config(config_path)
+
+        if not os.path.isfile(exe_path):
+            print("Getting Tor...")
+            self.download_tor()
 
         process = subprocess.Popen(f"{exe_path} -nt-service -f {config_path}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         while 1:
